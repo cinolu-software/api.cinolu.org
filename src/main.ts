82bc008eca9b74ad453eb42885b0bbeba5472e83
Wilfried Musanzi <musanziw.m@gmail.com>
2024-09-10 13:00:48 +0200

    feat: fix cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
-    allowedHeaders: ['Content-Type'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true
   });
