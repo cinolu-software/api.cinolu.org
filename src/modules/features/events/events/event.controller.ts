dc0f0160c85c6ad8f4fc62d5ec527192a4dbc5e5
Wilfried Musanzi <musanziw.m@gmail.com>
2024-11-07 11:41:45 +0200

    chore: fix typo om route name

--- src/modules/features/events/events/event.controller.ts
+++ src/modules/features/events/events/event.controller.ts
@@ -20,25 +20,25 @@ import { v4 as uuidv4 } from 'uuid';
 import { Public } from 'src/common/decorators/public.decorator';
 import { QueryParams } from './types/query-params.type';
 
-@Controller('event')
+@Controller('events')
 export class EventsController {
-  constructor(private readonly programsService: EventsService) {}
+  constructor(private readonly eventsService: EventsService) {}
 
   @Post('')
   create(@Body() createProgramDto: CreateEventDto): Promise<{ data: Event }> {
-    return this.programsService.create(createProgramDto);
+    return this.eventsService.create(createProgramDto);
   }
 
   @Public()
   @Get('')
   findAll(@Query() queryParams: QueryParams): Promise<{ data: { events: Event[]; count: number } }> {
-    return this.programsService.findAll(queryParams);
+    return this.eventsService.findAll(queryParams);
   }
 
   @Public()
   @Get(':id')
   findOne(@Param('id') id: string): Promise<{ data: Event }> {
-    return this.programsService.findOne(id);
+    return this.eventsService.findOne(id);
   }
 
   @Post('image/:id')
@@ -53,7 +53,7 @@ export class EventsController {
     })
   )
   uploadImage(@Param('id') id: string, @UploadedFile() file: Express.Multer.File): Promise<{ data: Event }> {
-    return this.programsService.uploadImage(id, file);
+    return this.eventsService.uploadImage(id, file);
   }
 
   @Post('attachment/:id')
@@ -68,26 +68,26 @@ export class EventsController {
     })
   )
   addAttachment(@Param('id') id: string, @UploadedFile() file: Express.Multer.File): Promise<{ data: Event }> {
-    return this.programsService.addAttachment(id, file);
+    return this.eventsService.addAttachment(id, file);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProgramDto: UpdateEventDto): Promise<{ data: Event }> {
-    return this.programsService.update(id, updateProgramDto);
+    return this.eventsService.update(id, updateProgramDto);
   }
 
   @Delete('attachment/:id')
   removeAttachment(@Param('id') id: string): Promise<void> {
-    return this.programsService.removeAttachment(id);
+    return this.eventsService.removeAttachment(id);
   }
 
   @Delete('restore/:id')
   restore(@Param('id') id: string): Promise<void> {
-    return this.programsService.restore(id);
+    return this.eventsService.restore(id);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string): Promise<void> {
-    return this.programsService.remove(id);
+    return this.eventsService.remove(id);
   }
 }
