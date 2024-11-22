0deea6c225441f2bf1300547a2c935823eac42d0
Wilfried Musanzi <musanziw.m@gmail.com>
2024-11-22 15:17:15 +0200

    fix: remove seeds

--- src/@core/modules/database/seeds/user.seeder.ts
+++ src/@core/modules/database/seeds/user.seeder.ts
@@ -4,10 +4,6 @@ import * as bcrypt from 'bcrypt';
 import { faker } from '@faker-js/faker';
 import { User } from '../../users/users/entities/user.entity';
 import { Role } from '../../users/roles/entities/role.entity';
-import { Expertise } from '../../users/expertises/entities/expertise.entity';
-import { Position } from '../../users/positions/entities/position.entity';
-import { Detail } from '../../users/users/entities/detail.entity';
-import { Social } from '../../users/users/entities/social.entity';
 
 export default class UserSeeder implements Seeder {
   async run(dataSource: DataSource) {
@@ -31,85 +27,11 @@ export default class UserSeeder implements Seeder {
      */
     const userRepository = dataSource.getRepository(User);
     const roleRepository = dataSource.getRepository(Role);
-    const detailRepository = dataSource.getRepository(Detail);
-    const expertiseRepository = dataSource.getRepository(Expertise);
-    const positionRepository = dataSource.getRepository(Position);
-    const socialRepository = dataSource.getRepository(Social);
 
     ['admin', 'user', 'staff', 'coach'].map(async (role) => {
       await roleRepository.save({ name: role });
     });
 
-    const createExpertises = async (count: number): Promise<Expertise[]> => {
-      return await Promise.all(
-        Array(count)
-          .fill('')
-          .map(async () => {
-            return await expertiseRepository.save({
-              name: faker.person.jobTitle(),
-              description: faker.person.jobDescriptor()
-            });
-          })
-      );
-    };
-
-    const createPositions = async (count: number) => {
-      return await Promise.all(
-        Array(count)
-          .fill('')
-          .map(async () => {
-            return await positionRepository.save({
-              name: faker.person.jobType(),
-              description: faker.person.jobDescriptor()
-            });
-          })
-      );
-    };
-
-    const createSocials = async (socials = ['Instagram', 'LinkdedIn', 'Twitter']) => {
-      return await Promise.all(
-        socials.map(async (social) => {
-          return await socialRepository.save({
-            name: social,
-            link: faker.internet.url()
-          });
-        })
-      );
-    };
-
-    const createDetail = async () => {
-      const positions: Position[] = await createPositions(faker.number.int({ min: 1, max: 2 }));
-      const expertises: Expertise[] = await createExpertises(faker.number.int({ min: 1, max: 2 }));
-      const socials: Social[] = await createSocials();
-      return await detailRepository.save({
-        bio: faker.person.bio(),
-        socials,
-        expertises,
-        positions
-      });
-    };
-
-    const createUsers = async (count: number, roleName: string, password: string) => {
-      await Promise.all(
-        Array(count)
-          .fill('')
-          .map(async () => {
-            await userRepository.save({
-              name: faker.person.fullName(),
-              address: faker.location.streetAddress(),
-              phone_number: faker.phone.number(),
-              email: faker.internet.email(),
-              verified_at: new Date(),
-              password: await bcrypt.hash(password, 10),
-              detail: await createDetail(),
-              roles: [await roleRepository.findOneByOrFail({ name: roleName })]
-            });
-          })
-      );
-    };
-
-    await createUsers(10, 'staff', 'staff1234');
-    await createUsers(20, 'coach', 'coach1234');
     await userRepository.save({
       name: faker.person.firstName(),
       address: faker.location.streetAddress(),
