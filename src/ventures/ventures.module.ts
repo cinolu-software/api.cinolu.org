c2d041687df00ce2e315371201b9e81b56553708
Wilfried Musanzi <musanziw.m@gmail.com>
2024-12-09 14:37:48 +0200

    feat: add SectorsController and SectorsService to VenturesModule

--- src/ventures/ventures.module.ts
+++ src/ventures/ventures.module.ts
@@ -4,10 +4,12 @@ import { Sector } from './entities/sectors.entity';
 import { Venture } from './entities/venture.entity';
 import { VenturesController } from './controllers/ventures.controller';
 import { VenturesService } from './services/ventures.service';
+import { SectorsController } from './controllers/sectors.controller';
+import { SectorsService } from './services/sectors.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Venture, Sector])],
-  controllers: [VenturesController],
-  providers: [VenturesService]
+  controllers: [VenturesController, SectorsController],
+  providers: [VenturesService, SectorsService]
 })
 export class VenturesModule {}
