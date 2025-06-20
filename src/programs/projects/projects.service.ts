5c60a3ee8e0cc551b5b1955e95bc58cd8dfb80da
Wilfried Musanzi <musanziw.m@gmail.com>
2025-06-20 08:54:23 +0200

    refactor: remove debug log from create method in ProjectsService

--- src/programs/projects/projects.service.ts
+++ src/programs/projects/projects.service.ts
@@ -16,8 +16,6 @@ export class ProjectsService {
 
   async create(dto: CreateProjectDto): Promise<Project> {
     try {
-      console.log('Creating project with DTO:', dto);
-      return;
       await this.throwIfExist(dto.name);
       return await this.projectRepository.save({
         ...dto,
