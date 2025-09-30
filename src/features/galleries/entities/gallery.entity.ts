8afe6d1799229b5bec712a3c3038cc5b973f1db9
Wilfried Musanzi <musanziw.m@gmail.com>
2025-09-30 10:25:02 +0200

    feat(gallery): add relationships for product and venture entities

--- src/features/galleries/entities/gallery.entity.ts
+++ src/features/galleries/entities/gallery.entity.ts
@@ -2,6 +2,8 @@ import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 import { BaseEntity } from '../../../shared/utils/abstract.entity';
 import { Project } from '../../programs/subprograms/projects/entities/project.entity';
 import { Event } from '../../programs/subprograms/events/entities/event.entity';
+import { Product } from '../../ventures/products/entities/product.entity';
+import { Venture } from '../../ventures/entities/venture.entity';
 
 @Entity()
 export class Gallery extends BaseEntity {
@@ -15,4 +17,13 @@ export class Gallery extends BaseEntity {
   @ManyToOne(() => Event)
   @JoinColumn()
   event: Event;
+
+
+  @ManyToOne(() => Product)
+  @JoinColumn()
+  product: Product;
+
+  @ManyToOne(() => Venture)
+  @JoinColumn()
+  venture: Venture;
 }
