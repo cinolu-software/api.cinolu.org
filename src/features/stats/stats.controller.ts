ec9f34829a7717ce7e2041493ac985e70c60a4ba
Wilfried Musanzi <musanziw.m@gmail.com>
2025-10-30 09:28:13 +0200

    refactor: remove unused admin stats logic and related methods from StatsService and StatsController

--- src/features/stats/stats.controller.ts
+++ src/features/stats/stats.controller.ts
@@ -1,20 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { StatsService } from './stats.service';
-import { UseRoles } from 'nest-access-control';
 import { IUSerStats } from './types/user-stats.type';
-import { IAdminStats } from './types/admin-stats.type';
 import { CurrentUser } from '../../core/auth/decorators/current-user.decorator';
 import { User } from '../../core/users/entities/user.entity';
 
 @Controller('stats')
 export class StatsController {
   constructor(private statsService: StatsService) {}
-
-  @Get('admin')
-  @UseRoles({ resource: 'stats', action: 'read' })
-  async findAll(): Promise<IAdminStats> {
-    return await this.statsService.findAdminStats();
-  }
 
   @Get('user')
   async findUserStats(@CurrentUser() user: User): Promise<IUSerStats> {
--- src/features/stats/stats.service.ts
+++ src/features/stats/stats.service.ts
@@ -1,70 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { User } from '../../core/users/entities/user.entity';
-import { Article } from '../blog/articles/entities/article.entity';
 import { Venture } from '../ventures/entities/venture.entity';
-import { Program } from '../programs/entities/program.entity';
-import { Subprogram } from '../programs/subprograms/entities/subprogram.entity';
-import { Project } from '../programs/subprograms/projects/entities/project.entity';
-import { Event } from '../programs/subprograms/events/entities/event.entity';
-import { IAdminStats } from './types/admin-stats.type';
 import { IUSerStats } from './types/user-stats.type';
 
 @Injectable()
 export class StatsService {
   constructor(private readonly dataSource: DataSource) {}
 
-  async findAdminStats(): Promise<IAdminStats> {
-    const [totalUsers, totalPrograms, totalSubprograms, totalProjects, totalEvents, totalArticles] = await Promise.all([
-      this.countUsers(),
-      this.countPrograms(),
-      this.countSubprograms(),
-      this.countProjects(),
-      this.countEvents(),
-      this.countArticles()
-    ]);
-
-    return {
-      totalUsers,
-      totalPrograms,
-      totalSubprograms,
-      totalProjects,
-      totalEvents,
-      totalArticles
-    };
-  }
-
   async findUserStats(user: User): Promise<IUSerStats> {
     const [totalVentures, referralsCount] = await Promise.all([
       this.countUserVentures(user.id),
       this.countUserReferrals(user.id)
     ]);
-
     return { totalVentures, referralsCount };
-  }
-
-  private async countUsers(): Promise<number> {
-    return await this.dataSource.getRepository(User).count();
-  }
-
-  private async countPrograms(): Promise<number> {
-    return await this.dataSource.getRepository(Program).count();
-  }
-
-  private async countSubprograms(): Promise<number> {
-    return await this.dataSource.getRepository(Subprogram).count();
-  }
-
-  private async countProjects(): Promise<number> {
-    return await this.dataSource.getRepository(Project).count();
-  }
-
-  private async countEvents(): Promise<number> {
-    return await this.dataSource.getRepository(Event).count();
-  }
-
-  private async countArticles(): Promise<number> {
-    return await this.dataSource.getRepository(Article).count();
   }
 
   private async countUserVentures(userId: string): Promise<number> {
