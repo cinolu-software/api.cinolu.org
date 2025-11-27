1073eb5dda3009a7c17000bab17ba1beb1a3d99f
Wilfried Musanzi <musanziw.m@gmail.com>
2025-11-27 15:00:37 +0200

    refactor(submissions): simplify CreateSubmissionDto by removing optional fields

--- src/modules/programs/projects/phases/submissions/dto/create-submission.dto.ts
+++ src/modules/programs/projects/phases/submissions/dto/create-submission.dto.ts
@@ -1,17 +1,8 @@
-import { IsArray, IsOptional } from 'class-validator';
+import { IsArray } from 'class-validator';
 
 export class CreateSubmissionDto {
   formId: string;
 
   @IsArray()
-  responses: { label: string; value: string }[];
-
-  @IsOptional()
-  submitted_by_name?: string;
-
-  @IsOptional()
-  submitted_by_email?: string;
-
-  @IsOptional()
-  submitted_by_phone?: string;
+  responses: Record<string, unknown>[];
 }
