8f349fc67b9a19a802ac888cc2018caa55f38d1e
Wilfried Musanzi <musanziw.m@gmail.com>
2026-01-08 16:27:56 +0200

    refactor(user): remove MentorsModule from UsersModule imports for cleaner structure

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -6,10 +6,9 @@ import { EmailModule } from '@/core/email/email.module';
 import { UsersService } from './users.service';
 import { RolesModule } from './roles/roles.module';
 import { UserSubscriber } from './subscribers/user.subscriber';
-import { MentorsModule } from './mentors/mentors.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), EmailModule, RolesModule, MentorsModule],
+  imports: [TypeOrmModule.forFeature([User]), EmailModule, RolesModule],
   controllers: [UsersController],
   providers: [UsersService, UserSubscriber],
   exports: [UsersService]
