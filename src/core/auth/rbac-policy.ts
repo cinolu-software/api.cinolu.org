6e72af9dd11dc0bbfd10790b35dbc2bb2b66e477
Wilfried Musanzi <musanziw.m@gmail.com>
2026-01-08 16:28:45 +0200

    refactor(auth): update RBAC policy to include mentorsProfiles and adjust permissions for user roles

--- src/core/auth/rbac-policy.ts
+++ src/core/auth/rbac-policy.ts
@@ -5,15 +5,16 @@ export const RBAC_POLICY: RolesBuilder = new RolesBuilder();
 
 RBAC_POLICY.grant(Role.USER)
   .readOwn(['products', 'ventures'])
-  .updateOwn(['ventures', 'comments', 'products', 'mentors', 'addCV'])
-  .deleteOwn(['ventures', 'comments', 'products', 'mentors'])
+  .updateOwn(['ventures', 'comments', 'products', 'mentorsProfiles', 'addCV'])
+  .deleteOwn(['ventures', 'comments', 'products', 'mentorsProfiles'])
+  .grant(Role.MENTOR)
+  .extend(Role.USER)
   .grant(Role.STAFF)
   .readAny([
     'stats',
     'users',
     'programs',
-    'mentors',
-    'expertises',
+    'mentorsProfiles',
     'subprograms',
     'roles',
     'comments',
@@ -30,6 +31,7 @@ RBAC_POLICY.grant(Role.USER)
   .createAny([
     'indicators',
     'users',
+    'expertises',
     'programs',
     'comments',
     'events',
@@ -77,7 +79,7 @@ RBAC_POLICY.grant(Role.USER)
     'blogs',
     'ventures',
     'galleries',
-    'mentors',
+    'mentorsProfiles',
     'expertises'
   ])
   .grant(Role.ADMIN)
