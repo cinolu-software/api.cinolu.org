c616dc4b155ee2b86d1e2a6e6d389a31e48af7cb
Wilfried Musanzi <musanziw.m@gmail.com>
2026-02-04 09:49:56 +0200

    refactor(auth): use ConfigService for Google OAuth credentials in GoogleStrategy

--- src/core/auth/strategies/google.strategy.ts
+++ src/core/auth/strategies/google.strategy.ts
@@ -3,14 +3,18 @@ import { Profile, Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { Injectable } from '@nestjs/common';
 import { AuthService } from '../auth.service';
 import { CreateWithGoogleDto } from '../dto/sign-up-with-google.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-  constructor(private authService: AuthService) {
+  constructor(
+    private authService: AuthService,
+    configService: ConfigService
+  ) {
     super({
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-      callbackURL: process.env.GOOGLE_REDIRECT_URI,
+      clientID: configService.get('GOOGLE_CLIENT_ID'),
+      clientSecret: configService.get('GOOGLE_SECRET'),
+      callbackURL: configService.get('GOOGLE_REDIRECT_URI'),
       scope: ['email', 'profile']
     });
   }
