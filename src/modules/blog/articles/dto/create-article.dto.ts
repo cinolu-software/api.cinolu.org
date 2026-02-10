5ad9eb43e84564e251bd4d8e6d18f8576ad5442b
Wilfried Musanzi <musanziw.m@gmail.com>
2026-02-10 13:14:34 +0200

    fix(dto): add missing semicolon in CreateArticleDto for consistency

--- src/modules/blog/articles/dto/create-article.dto.ts
+++ src/modules/blog/articles/dto/create-article.dto.ts
@@ -2,7 +2,7 @@ import { IsArray, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class CreateArticleDto {
   @IsOptional()
-  published_at: string
+  published_at: string;
 
   @IsNotEmpty()
   title: string;
