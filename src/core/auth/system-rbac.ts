85c7ce531d993c3e7e8bf2122a8b5db0f85a9d59
Wilfried Musanzi <musanziw.m@gmail.com>
2026-03-11 13:09:50 +0200

    refactor(rbac): remove SYSTEM_RBAC_POLICY as part of cleanup

--- src/core/auth/system-rbac.ts
+++ src/core/auth/system-rbac.ts
@@ -1,4 +1,4 @@
-import { Role } from '../enums/roles.enum';
+import { Role } from './enums/roles.enum';
 import { ModuleRbacPolicy } from '@musanzi/nestjs-session-auth';
 
 export const SYSTEM_RBAC_POLICY: ModuleRbacPolicy = {
